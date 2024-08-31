69561a16af744571af2b28f963f32087b00628d2
nayanagc <nnayanagc@gmail.com>
2024-08-31 13:07:56 +0530

    Create swap_ref.c

---
+++ swap_ref.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void swap(int *x, int *y)
+{  int temp=*x;
+  *x=*y;
+  *y=temp;}
+void main()
+{  int a=10,b=20;
+  printf("Before swapping a=%d,b=%d\n",a,b);
+  swap(&a,&b);
+  printf("After swapping a=%d,b=%d",a,b); }
